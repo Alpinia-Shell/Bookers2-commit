eac3df5369bf8387c9fa4ed45756bda4cdd5c834
EC2 Default User <ec2-user@ip-172-31-11-93.ap-northeast-1.compute.internal>
2025-04-01 13:05:31 +0000

    [Add]ルートディレクトリの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'homes#home'
+  get 'home/about' => 'homes#about'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
