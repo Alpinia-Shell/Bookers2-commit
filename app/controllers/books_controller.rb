93da6dd9a476148987d181868500c81ec2b1a33c
Alpinia-Shell <strawberryscratch@gmail.com>
2025-04-02 09:18:18 +0000

    [stop]更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,23 @@
 class BooksController < ApplicationController
-  def new
+  def index 
+    # @book = Book.find(params[:id])
+    @book = Book.new
   end
 
-  def index
-  end
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end 
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about' ,as: 'about'
-  resources :books, only:[:edit, :show, :index]
-  resources :users, only:[:index,:show]
+  resources :books, only:[:edit, :create, :show, :index]
   devise_for :users
+  resources :users, only:[:index,:show]
+
+  
   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
