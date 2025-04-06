e8b54466996476c07e01e76e833706a77f12d497
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-04-06 07:45:59 +0000

    [Add]ユーザーページ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
 
   def  index
+    @user = current_user
+    @book = Book.new
+    @users = User.all
   end
 
   def  show
+    @user = current_user
+    @book = Book.new
+    @books = current_user.books
   end
 end
