74c793d12514752701db72e49415c1a47590cac7
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-04-06 13:27:01 +0000

    [finish]今日は終わり

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about' ,as: 'about'
-  resources :books, only:[:new, :edit, :create, :show, :index,:update, :destroy]
+  resources :books
   devise_for :users
   resources :users, only:[:index,:show, :update]
-
-  
-  
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
