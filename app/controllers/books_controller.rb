2a8bf1841ff0f2ea9ac9d5d618bae1cbb796194e
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-04-08 12:58:07 +0000

    [Add]ユーザー権限追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
      redirect_to book_path(@book)
     else
     @user = current_user
@@ -24,12 +25,22 @@ class BooksController < ApplicationController
   end
 
   def edit
+    book = Book.find(params[:id])
+    unless
+    book.user == current_user
+    redirect_to books_path
+    end
     @book = Book.find(params[:id])
-  end
+  end 
 
   def update
     @book = Book.find(params[:id])
+    unless
+    @book.user == current_user
+    redirect_to books_path
+    end 
     if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book)
     else
     render :edit
