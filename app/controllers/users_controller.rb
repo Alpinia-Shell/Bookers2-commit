a88919de4c179328548c55bbc77219687301eebf
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-04-08 07:22:57 +0000

    [Add]バリデーション設定完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,23 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end 
-
   def  show
     @user = User.find(params[:id])
     @book = Book.new
     @books = @user.books
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end 
+
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
+    if @user.update(user_params)
     redirect_to user_path(@user)
+    else
+    render :edit
+    end 
   end
 
   private
